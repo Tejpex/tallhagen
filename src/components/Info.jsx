eca4cf6f298121024023ccfbb74dd5803937d728
Tejpex <equis_angel@hotmail.com>
2024-04-07 12:06:20 +0200

    Used mark-tag to highlight some words

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -17,24 +17,21 @@ export const Info = () => {
       {/*<p>Skäggenäs</p>*/}
       <p>En plats nära Kalmar</p>
       <h2>För vem?</h2>
-      <p className="bold">Upptäckare, äventyrare, utmanare, roverscouter och ledare</p>
-      <p>- tors-sön</p>
-      <div className="same-row">
-        <p className="bold">Spårare</p>
-        <p>- fre-sön</p>
-      </div>
-      <div className="same-row">
-        <p className="bold">Familjescouter</p>
-        <p>- lördag eftermiddag</p>
-      </div>
+      <p>
+        <mark>Upptäckare, äventyrare, utmanare, roverscouter och ledare</mark> -
+        tors-sön <br />
+        <mark>Spårare</mark> - fre-sön <br />
+        <mark>Familjescouter</mark> - lördag eftermiddag
+      </p>
       <h2>Kostnad</h2>
       <p>
-        Spårare: 550kr <br/> Upptäckare, äventyrare, utmanare, roverscouter: 650kr <br/>
-        Ledare/funktionär: Gratis, som tack för att du hjälper till.
+        <mark>Spårare:</mark> 550kr<br /> 
+        <mark>Upptäckare, äventyrare, utmanare, roverscouter:</mark> 650kr<br />
+        <mark>Ledare/funktionär:</mark> Gratis, som tack för att du hjälper till.
       </p>
       <h2>När anmäler jag mig?</h2>
-      <p>Redan nu! Men senast torsdag den 9 maj.</p>
+      <p>Redan nu! Men senast <mark>torsdag den 9 maj.</mark></p>
       {/*<p>Sista avanmälningsdag är 23 maj.</p>*/}
     </div>
-  );
+  )
 }
