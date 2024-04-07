8611c0bb92ed108e7c1e0f44520ca419dd01052c
Tejpex <equis_angel@hotmail.com>
2024-04-07 09:06:54 +0200

    Added more information

---
+++ src/components/Equipment.jsx
@@ -0,0 +1,14 @@
+
+
+export const Equipment = () => {
+  return (
+    <div>
+        <h2>Packning</h2>
+        <p>Här är en packlista som du kan följa.</p>
+        <ul>
+            <li>Liggunderlag</li>
+            <li>Sovsäck</li>
+        </ul>
+    </div>
+  )
+}
--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -4,10 +4,12 @@ export const Info = () => {
   return (
     <div className="info-box">
       <p className="bold">
-        Nu är det dags att dyka ner under ytan med Tallhagens Scoutkår! 
+        Nu är det dags att dyka ner under ytan med Tallhagens Scoutkår!
       </p>
       <p>
-        Årets sommarläger har temat &quot;Den lilla sjöjungfrun&quot;, så häng med Sebastian och gänget, ta med dina scoutkompisar och upptäck att 🎶 havet är djupt, havet är djupt... 🎵
+        Årets sommarläger har temat &quot;Den lilla sjöjungfrun&quot;, så häng
+        med Sebastian och gänget, ta med dina scoutkompisar och upptäck att 🎶
+        havet är djupt, havet är djupt... 🎵
       </p>
       <h2>När?</h2>
       <p>6-9 juni 2024</p>
@@ -15,10 +17,24 @@ export const Info = () => {
       {/*<p>Skäggenäs</p>*/}
       <p>En plats nära Kalmar</p>
       <h2>För vem?</h2>
-      <p className="bold">Upptäckare, Äventyrare, Utmanare, Rover och ledare</p> 
+      <p className="bold">Upptäckare, äventyrare, utmanare, roverscouter och ledare</p>
       <p>- tors-sön</p>
-      <p className="bold">Spårare</p>
-      <p>- fre-sön</p>
+      <div className="same-row">
+        <p className="bold">Spårare</p>
+        <p>- fre-sön</p>
+      </div>
+      <div className="same-row">
+        <p className="bold">Familjescouter</p>
+        <p>- lördag eftermiddag</p>
+      </div>
+      <h2>Kostnad</h2>
+      <p>
+        Spårare: 550kr <br/> Upptäckare, äventyrare, utmanare, roverscouter: 650kr <br/>
+        Ledare/funktionär: Gratis, som tack för att du hjälper till.
+      </p>
+      <h2>När anmäler jag mig?</h2>
+      <p>Redan nu! Men senast torsdag den 9 maj.</p>
+      {/*<p>Sista avanmälningsdag är 23 maj.</p>*/}
     </div>
   );
 }
