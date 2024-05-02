b13f77e852ede0d4b2b28a424c6bce6440bcc48e
Tejpex <equis_angel@hotmail.com>
2024-05-02 22:01:28 +0200

    Added location

--- src/components/Equipment.jsx
+++ src/components/Equipment.jsx
@@ -3,12 +3,65 @@
 export const Equipment = () => {
   return (
     <div>
-        <h2>Packning</h2>
-        <p>Här är en packlista som du kan följa.</p>
-        <ul>
-            <li>Liggunderlag</li>
-            <li>Sovsäck</li>
-        </ul>
+      <h2>Packning</h2>
+      <p>
+        Packlista för helgläger Packa alltid kläder efter årstid – man ska kunna
+        vara ute i alla väder utan att frysa eller bli blöt. Tänk också på att
+        det är oömma kläder som gäller – kläder som går att smutsa ner och är
+        fria att leka och röra sig i. Skor:
+      </p>
+      <p>Här är en packlista som du kan följa.</p>
+      <ul> Sova:
+        <li>Liggunderlag</li>
+        <li>Sovsäck</li>
+        <li>Pyamas eller liknande</li>
+      </ul>
+      <ul> Kläder:
+        <li>Underkläder (Kalsonger/ Trosor + ev. BH)</li>
+        <li>Strumpor</li>
+        <li>Raggsockar</li>
+        <li>Byxor</li>
+        <li>T-shirts/ Linnen eller liknande</li>
+        <li>Tjockare tröjor</li>
+        <li>Jacka efter årstid</li>
+        <li>Regnjacka</li>
+        <li>Regnbyxor</li>
+        <li>Vantar</li>
+        <li>Mössa</li>
+      </ul>
+      <ul> Äta
+        <li>Kåsa/mugg</li>
+        <li>Djup tallrik</li>
+        <li>Bestick</li>
+        <li>Diskhandduk</li>
+        <li>Vattenflaska med vatten</li>
+      </ul>
+      <ul> Hygien
+        <li>Tandborste</li>
+        <li>Tandkräm</li>
+        <li>Kam/Hårborste</li>
+        <li>Tvål</li>
+        <li>Liten handduk</li>
+      </ul>
+         
+         
+            Eventuellt: Halsduk Långkalsonger Och om du vill: Kudde
+      Sovsäckslakan Gosedjur Äta: Matsaker (, , )
+        Tvätta sig: Necessär 
+          Vid behov: Mediciner Hudlotion
+      Deodorant Trosskydd/Bindor/Tamponger Snoddar + Hårspännen Skavsårsplåster
+      Övrigt: Penna + Papper Kniv (om du har knivbevis) Och om du vill:
+      Ficklampa Kamera + Batterier Sittunderlag Lägerbålsfilt Nål och tråd Om
+      lägret är på sommaren: Shorts eller Kjol Sandaler eller liknande Myggmedel
+      Solkräm Badkläder Badhandduk Skor: Gummistövlar + Skor att leka ute i, som
+      passar årstiden eller Vandrarkängor Viktigt att tänka på med skor är att
+      man ska kunna gå en längre sträcka utan att bli blöt om fötterna, så ett
+      par vattentåliga, men samtidigt bekväma skor är det bästa alternativet.
+      Packa i: Stor ryggsäck/Bag Liten ryggsäck Oftast gör vi dagsutfärder då
+      alla måste ha en liten ryggsäck med regnkläder, vattenflaska och matsaker
+      i. Reseklädsel: Scoutskjorta, Scouthalsduk och Sölja om du har det. I
+      övrigt: kläder efter väder. Man ska alltid kunna bära all sin packning
+      själv!
     </div>
-  )
+  );
 }
--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -4,7 +4,10 @@ export const Info = () => {
   return (
     <div className="info-box">
       <p className="bold">
-        <mark>Nu är det dags att dyka ner under ytan med Tallhagens Scoutkår!</mark> <br />
+        <mark>
+          Nu är det dags att dyka ner under ytan med Tallhagens Scoutkår!
+        </mark>{" "}
+        <br />
         Årets sommarläger har temat &quot;Den lilla sjöjungfrun&quot;, så häng
         med Sebastian och gänget, ta med dina scoutkompisar och upptäck att 🎶
         havet är djupt, havet är djupt... 🎵
@@ -12,8 +15,7 @@ export const Info = () => {
       <h2>När?</h2>
       <p>6-9 juni 2024</p>
       <h2>Var?</h2>
-      {/*<p>Skäggenäs</p>*/}
-      <p>En plats nära Kalmar</p>
+      <p>Varvet på Skäggenäs -<a href="https://maps.app.goo.gl/TU6w9eTrMSqpfdjD8">Karta</a></p>
       <h2>För vem?</h2>
       <p>
         <mark>Upptäckare, äventyrare, utmanare, roverscouter och ledare</mark> -
@@ -32,14 +34,20 @@ export const Info = () => {
       </p>
       <h2>När anmäler jag mig?</h2>
       <p>
-        Redan nu! Men senast <mark>torsdag den 9 maj.</mark> Detta datum ska också betalningen ha kommit in.
+        Redan nu! Men senast <mark>torsdag den 9 maj.</mark> Detta datum ska
+        också betalningen ha kommit in.
       </p>
       {/*<p>Sista avanmälningsdag är 23 maj.</p>*/}
       <h2>Hjälp</h2>
       <p>
-        <mark>Scouter gör saker tillsammans!</mark> Planeringen är i full gång, men vi har alltid behov av fler händer som kan hjälpa till innan, efter eller under lägret. Vill du bidra med något? Hör av dig till en ledare, så kan vi berätta mer om vilka arbetsuppgifter som finns. <br />
-        Vi kommer också be er föräldrar till anmälda barn att hjälpa till med något under lägrets gång. Mer information om detta finns i anmälningsformuläret. Tack på förhand!
+        <mark>Scouter gör saker tillsammans!</mark> Planeringen är i full gång,
+        men vi har alltid behov av fler händer som kan hjälpa till innan, efter
+        eller under lägret. Vill du bidra med något? Hör av dig till en ledare,
+        så kan vi berätta mer om vilka arbetsuppgifter som finns. <br />
+        Vi kommer också be er föräldrar till anmälda barn att hjälpa till med
+        något under lägrets gång. Mer information om detta finns i
+        anmälningsformuläret. Tack på förhand!
       </p>
     </div>
-  )
+  );
 }
