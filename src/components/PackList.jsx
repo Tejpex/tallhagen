9b7dec679cc0d731aeb403fd870599ae4489e183
Erika Wernbro <155359298+Tejpex@users.noreply.github.com>
2025-07-23 11:32:53 +0200

    Restructure PackList

--- src/components/PackList.jsx
+++ src/components/PackList.jsx
@@ -4,9 +4,28 @@ import { useEquipment } from "../contexts/EquipmentContext"
 //import IceCream from "/ice-cream.png"
 
 export const PackList = () => {
+  const isAHike = false
   const { equipment, completeAll } = useEquipment()
-  const itemsToPack = equipment.filter((item) => item.done === false)
-
+  const itemsToPack = equipment.filter((item) => {
+    if (item.done === true) return false
+    return isAHike ? item.hike === true : true
+  })
+  const myOwnItems = equipment.filter(
+          (item) => item.category === "My own" && item.done === false
+        )
+  const itemsSleep = itemsToPack.filter((item) => item.category === "Sova") 
+  const itemsSleepExtra = itemsToPack.filter((item) => item.category === "Extra-sov")
+  const itemsClothing = itemsToPack.filter((item) => item.category === "Kläder")
+  const itemsClothingExtra = itemsToPack.filter((item) => item.category === "Extra-kläder")
+  const itemsShoes = itemsToPack.filter((item) => item.category === "Skor")
+  const itemsMealkit = itemsToPack.filter((item) => item.category === "Äta")
+  const itemsHygiene = itemsToPack.filter((item) => item.category === "Hygien")
+  const itemsIfNeeded = itemsToPack.filter((item) => item.category === "Vid behov")
+  const itemsMisc = itemsToPack.filter((item) => item.category === "Övrigt")
+  const itemsIfWanted = itemsToPack.filter((item) => item.category === "Om du vill")
+  const itemsBags = itemsToPack.filter((item) => item.category === "Väskor")
+  const itemsSummer = itemsToPack.filter((item) => item.category === "Sommarsaker")
+          
   if (itemsToPack.length === 0) {
     return (
       <div className="info-box">
@@ -35,43 +54,27 @@ export const PackList = () => {
         <h3>Här är en packlista som du kan följa:</h3>
         <p className="site-info">Du kan markera sakerna allt eftersom du packar dem. Möjligheten finns också att ta bort och lägga till saker för att göra listan personlig. Dina ändringar sparas dock inte. Vill du göra det behöver du skriva ut listan.</p>
         <h4>Sova</h4>
-        {itemsToPack
-          .filter((item) => item.hike === true && item.category === "Sova")
-          .map((item) => (
+        {itemsSleep.map((item) => (
             <Item key={item.id} item={item} />
           ))}
         <h4>Om du behöver</h4>
-        {itemsToPack
-          .filter(
-            (item) => item.hike === true && item.category === "Extra-sov"
-          )
-          .map((item) => (
+        {itemsSleepExtra.map((item) => (
             <Item key={item.id} item={item} />
           ))}
         <h4>Kläder</h4>
         <p>Ha gärna på dig scoutskjorta när vi reser till lägret.</p>
-        {itemsToPack
-          .filter((item) => item.hike === true && item.category === "Kläder")
-          .map((item) => (
+        {itemsClothing.map((item) => (
             <Item key={item.id} item={item} />
           ))}
         <h4>Om det är kallt (tänk även på temperaturen på kvällar/nätter)</h4>
-        {itemsToPack
-          .filter(
-            (item) => item.hike === true && item.category === "Extra-kläder"
-          )
-          .map((item) => (
+        {itemsClothingExtra.map((item) => (
             <Item key={item.id} item={item} />
           ))}
         <h4>Skor</h4>
-        {itemsToPack
-          .filter((item) => item.hike === true && item.category === "Skor")
-          .map((item) => (
+        {itemsShoes.map((item) => (
             <Item key={item.id} item={item} />
           ))}
-        {itemsToPack.filter(
-          (item) => item.hike === true && item.category === "Skor"
-        ).length > 0 && (
+        {itemsShoes.length > 0 && (
           <p>
             Du måste kunna gå en längre sträcka utan att bli blöt om fötterna
             eller få skavsår, så ett par vattentåliga, men samtidigt bekväma
@@ -80,65 +83,39 @@ export const PackList = () => {
           </p>
         )}
         <h4>Matsaker</h4>
-        {itemsToPack
-          .filter((item) => item.hike === true && item.category === "Äta")
-          .map((item) => (
+        {itemsMealkit.map((item) => (
             <Item key={item.id} item={item} />
           ))}
         <h4>Hygien</h4>
-        {itemsToPack
-          .filter((item) => item.hike === true && item.category === "Hygien")
-          .map((item) => (
+        {itemsHygiene.map((item) => (
             <Item key={item.id} item={item} />
           ))}
         <h4>Vid behov</h4>
-        {itemsToPack
-          .filter(
-            (item) => item.hike === true && item.category === "Vid behov"
-          )
-          .map((item) => (
+        {itemsIfNeeded.map((item) => (
             <Item key={item.id} item={item} />
           ))}
         <h4>Övrigt</h4>
-        {itemsToPack
-          .filter((item) => item.hike === true && item.category === "Övrigt")
-          .map((item) => (
+        {itemsMisc.map((item) => (
             <Item key={item.id} item={item} />
           ))}
         <h4>Om du vill</h4>
-        {itemsToPack
-          .filter(
-            (item) => item.hike === true && item.category === "Om du vill"
-          )
-          .map((item) => (
+        {itemsIfWanted.map((item) => (
             <Item key={item.id} item={item} />
           ))}
         <p>Tänk på att elektronisk utrustning är känslig för väder och vind, och att det är svårt att hitta ett eluttag i naturen. Om en scout vill ringa hem, kan de låna en telefon av oss ledare. Vårdnadshavare kan också ringa till en ledare om de vill säga något till sin scout.</p>
         <h4>Eftersom lägret är på sommaren 🌞</h4>
-        {itemsToPack
-          .filter(
-            (item) => item.hike === true && item.category === "Sommarsaker"
-          )
-          .map((item) => (
+        {itemsSummer.map((item) => (
             <Item key={item.id} item={item} />
           ))}
-        {itemsToPack.filter(
-          (item) => item.category === "My own"
-        ).length > 0 && <h4>Mina egna saker</h4>}
-        {itemsToPack
-          .filter((item) => item.category === "My own")
-          .map((item) => (
+        {myOwnItems.length > 0 && <h4>Mina egna saker</h4>}
+        {myOwnItems.map((item) => (
             <Item key={item.id} item={item} />
           ))}
         <h4>Packa i</h4>
-        {itemsToPack
-          .filter((item) => item.hike === true && item.category === "Väskor")
-          .map((item) => (
+        {itemsBags.map((item) => (
             <Item key={item.id} item={item} />
           ))}
-        {itemsToPack.filter(
-          (item) => item.hike === true && item.category === "Väskor"
-        ).length > 0 && (
+        {itemsBags.length > 0 && (
           <p>
             Oftast gör vi dagsutfärder och då måste alla ha en liten ryggsäck med
             regnkläder, vattenflaska och matsaker i.
